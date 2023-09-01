676d1a760691c2acaa4eae39179a1505bb009ddf
chaekeun <chaen328@khu.ac.kr>
2023-09-01 19:47:35 +0900

    api 오류없음

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,10 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
+const { ObjectId } = require("mongodb");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const cors = require("cors");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(
+  session({
+    secret: process.env.COOKIESECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -24,7 +36,7 @@ MongoClient.connect(
 );
 
 passport.serializeUser(function (user, done) {
-  done(null, user.id);
+  done(null, user._id);
 });
 
 passport.deserializeUser(function (id, done) {
@@ -57,11 +69,19 @@ passport.use(
 app.post(
   "/api/signin",
   passport.authenticate("local", {
-    successRedirect: `http://localhost:3000/home/${req.user._id}`,
     failureRedirect: "http://localhost:3000/register",
-    successFlash: "Welcome to chaek todo",
     failureFlash: true,
-  })
+  }),
+  (req, res) => {
+    req.login(req.user, (err) => {
+      if (err) {
+        console.error("Error loggint in: ", err);
+        res.redirect("http://localhost:3000/register");
+      } else {
+        res.redirect(`http://localhost:3000/tasks/${req.user._id}`);
+      }
+    });
+  }
 );
 
 app.post("/api/signup", (req, res) => {
@@ -85,11 +105,59 @@ app.get("/api/signout", (req, res) => {
 });
 
 app.get("/api/tasks/:user", async (req, res) => {
-  let userId = req.params.user;
+  let user_id = req.params.user;
+  console.log(user_id);
   await db
     .collection("post")
-    .find({ writer: new ObjectId(userId) })
+    .find({ writer: ObjectId(user_id) })
     .toArray((err, result) => {
       res.send(result);
     });
+});
+
+app.post("/api/complete/:id/:status", (req, res) => {
+  const updatedStatus = req.params.status === "true";
+  console.log(updatedStatus);
+  db.collection("post").updateOne(
+    { _id: ObjectId(req.params.id) },
+    { $set: { completed: !updatedStatus } },
+    (err, result) => {
+      res.send("success to update the status");
+    }
+  );
+});
+
+app.delete("/api/delete/:id", (req, res) => {
+  db.collection("post").deleteOne(
+    { _id: ObjectId(req.params.id) },
+    (err, result) => {
+      res.send("successfully delete data in mongodb");
+    }
+  );
+});
+
+app.post("/api/add", (req, res) => {
+  db.collection("post").insertOne(
+    {
+      title: req.body.title,
+      content: req.body.content,
+      created_at: req.body.created_at,
+      writer: req.user._id,
+      completed: false,
+    },
+    (err, result) => {
+      res.redirect(`http://localhost:3000/tasks/${req.user._id}`);
+    }
+  );
+});
+
+app.post("/api/edit", (req, res) => {
+  console.log(req.body.taskId);
+  db.collection("post").updateOne(
+    { _id: ObjectId(req.body.taskId) },
+    { $set: { title: req.body.title, content: req.body.content } },
+    (err, result) => {
+      res.redirect(`http://localhost:3000/tasks/${req.user._id}`);
+    }
+  );
 });
